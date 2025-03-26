72dad11d5e890c56bc023b1dafb3d19d79767519
HigorCarneiro <higorcarneiro2004@gmail.com>
2025-03-26 19:27:11 -0300

    .

---
+++ lista1/app/Http/Controllers/ExerciciosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExerciciosController extends Controller
+{
+    public function abrirFormExer1(){
+        return view('lista.ex1');
+    }
+
+    public function respostaExer1(Request $request){}
+}
